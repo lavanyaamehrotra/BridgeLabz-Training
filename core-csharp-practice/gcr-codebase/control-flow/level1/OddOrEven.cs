845164cad7dd48f718da741858e18867ea6634bd
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2025-12-22 16:15:40 +0530

    solved 2 more questions

--- core-csharp-practice/gcr-codebase/control-flow/level1/OddOrEven.cs
+++ core-csharp-practice/gcr-codebase/control-flow/level1/OddOrEven.cs
@@ -1,8 +1,4 @@
 // Create a program to print odd and even numbers between 1 to the number entered by the user.
-// Hint => 
-// Get an integer input from the user, assign to a variable number and check for Natural Number
-// Using a for loop, iterate from 1 to the number
-// In each iteration of the loop, print the number is odd or even number
 using System;
 class OddOrEven{
   static void Main(string [] args){
