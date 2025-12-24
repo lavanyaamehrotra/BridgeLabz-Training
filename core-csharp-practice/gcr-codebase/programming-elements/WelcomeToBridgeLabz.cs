079235ff7d1c55cab3c12adae55f323d9d10325d
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2025-12-24 14:47:29 +0530

    Remove core-csharp-practice folder from review branch

--- core-csharp-practice/gcr-codebase/programming-elements/WelcomeToBridgeLabz.cs
+++
@@ -1,8 +0,0 @@
-using System;
-namespace ProgrammingElements{
-    class WelcomeToBridgeLabz{
-        static void Main(string[] args){
-            Console.WriteLine("Welcome to BridgeLabz");
-        }
-    }
-}
