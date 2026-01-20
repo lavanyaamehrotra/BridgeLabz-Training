b020c2ad7d0739e0b9aa9ff7ae67194bf5a472a1
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2026-01-20 19:30:04 +0530

    [Lavanya Mehrotra] .add the  meal plan system

---
+++ generics-csharp-practice/gcr-codebase/meal-plan-system/VegetarianMeal.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class VegetarianMeal : IMealPlan{
+    public string Name { get; private set; }
+    public int Calories { get; private set; }
+    // constructor
+    public VegetarianMeal(string name, int calories){
+        Name = name;
+        Calories = calories;
+    }
+    //display
+    public void Display(){
+        Console.WriteLine($"[Vegetarian] {Name} - {Calories} kcal");
+    }
+}
