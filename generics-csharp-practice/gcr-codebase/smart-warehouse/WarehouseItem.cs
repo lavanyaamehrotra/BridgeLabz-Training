37beb2dd30184e8841e895b33c8d7dd0b337412d
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2026-01-20 19:39:31 +0530

    [Lavanya Mehrotra] .add the smaart warehouse

--- generics-csharp-practice/gcr-codebase/smart-warehouse/WarehouseItem.cs
+++ generics-csharp-practice/gcr-codebase/smart-warehouse/WarehouseItem.cs
@@ -3,14 +3,11 @@ public abstract class WarehouseItem{
     public int Id { get; set; }
     public string Name { get; set; }
     public int Quantity { get; set; }
-
     // constructor 
-    protected WarehouseItem(int id, string name, int quantity)
-    {
+    protected WarehouseItem(int id, string name, int quantity){
         Id = id;
         Name = name;
         Quantity = quantity;
     }
-
     public abstract void Display();
 }
